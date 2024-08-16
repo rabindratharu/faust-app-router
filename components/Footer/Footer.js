a5dd7602965e0f3a30aabdbf79d4b0a22f223d9b
Rakesh Shrestha <wildpilarak@gmail.com>
2024-08-16 09:16:36 +0545

    static navbar content in tailwind

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -9,7 +9,72 @@ export default function Footer({ title, menuItems }) {
 
   return (
     <footer className={cx('component')}>
-      <Container>
+        <div className='widget-area'>
+        <Container>
+          <div className="container px-3 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <aside className="widget pb-4">
+              <h3 className="capitalize text-md mb-4 font-semibold">Stay Connected</h3>
+              <div className="textarea p-0">
+                <p className="">Subscribe now and join with 6 Million+ users to get exclusive WordPress resources</p>
+              </div>
+            </aside>
+            <aside className="widget pb-4">
+              <h3 className="capitalize text-md mb-4 font-semibold">Products</h3>
+              <ul>
+                <li> 
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">about us</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">FAQs</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">blog</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">services</a>
+                </li>
+              </ul>
+            </aside>
+            <aside className="widget pb-4">
+              <h3 className="capitalize text-md mb-4 font-semibold">Support</h3>
+              <ul>
+                <li> 
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">about us</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">FAQs</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">blog</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">services</a>
+                </li>
+              </ul>
+            </aside>
+            <aside className="widget pb-4">
+              <h3 className="capitalize text-md mb-4 font-semibold">Company</h3>
+              <ul>
+                <li> 
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">about us</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">FAQs</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">blog</a>
+                </li>
+                <li>
+                  <a href="#" className="capitalize mb-2 inline-block  hover:text-orange-500">services</a>
+                </li>
+              </ul>
+            </aside>
+          </div>
+          </Container>
+        </div>
+
+
+        <Container>
         <NavigationMenu menuItems={menuItems} />
         <p className={cx('copyright')}>{`${title} © ${year}. Powered by WordPress.`}</p>
       </Container>
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -14,6 +14,46 @@ export default function Header({
   const [isNavShown, setIsNavShown] = useState(false);
 
   return (
+    <>
+    <header className={cx('component')}>
+      <SkipNavigationLink />
+        <Container className="flex items-center justify-between py-4">
+        <div className="site-branding">
+            <div class="site-title">
+              <Link href="/">
+                  <a target="_self" className='text-4xl uppercase font-bold'>
+                      logo
+                  </a>
+                </Link>
+            </div>
+        </div>
+        <div class="navbar bg-base-100 p-0">
+          <div class="primary-menu lg:flex w-full">
+            <ul class="menu menu-horizontal p-0 items-center w-full justify-end">
+              <li><a>Item 1</a></li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul class="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                  </ul>
+                </details>
+              </li>
+              <li><a>Item 3</a></li>
+              <li>
+              <a class="btn">Button</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </Container>
+      </header>
+
+    
+
+
+
     <header className={cx('component')}>
       <SkipNavigationLink />
         <Container>
@@ -41,5 +81,7 @@ export default function Header({
         </div>
       </Container>
     </header>
+    </>
+    
   );
 }
